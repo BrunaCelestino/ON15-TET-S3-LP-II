e1a55071ed51e67c7048fc81a0468d121f6b59c8
BrunaCelestino <brunaccelestino@gmail.com>
2022-04-11 23:10:08 -0300

    Exercício semana três

---
+++ repeticao.js
@@ -0,0 +1,26 @@
+// for (let i = 0; i <= 10; i++){
+//     let soma = i + i
+//     console.log(`número ${i}`)
+// }
+
+// for (let i = 0; i <= 10; i++){
+//     let soma = i + i
+//     console.log(`A soma de ${i} + ${i} é igual a ${soma}`)
+// }
+
+// const numbers = [1,20,19,18,5,100]
+// for (let i = 0; i < numbers.length; i++){
+//     const dobro = numbers[i] * 2
+//     console.log(`O dobro de ${numbers[i]} é igual a ${dobro}`)
+// }
+
+let y = 10
+while(y<10){
+    y++
+    console.log(y)
+}
+
+do{
+    y++
+    console.log(y)
+}while(y<10)
---
+++ revisao.js
@@ -0,0 +1,143 @@
+// if (horario >= 0 && horario < 6) {
+//     console.log("madrugada");
+// } else if (horario >= 6 && horario < 12) {
+//     console.log("manhã");
+// } else if (horario >= 12 && horario < 18) {
+//     console.log("tarde");
+// } else if (horario >= 18 &&  horario < 24) {
+//     console.log("noite");
+// } else {
+//     console.log("horário inválido");
+// }
+
+// let regiao = "sul"
+
+// switch (regiao){
+//     case "nordeste":
+//         console.log("Possui 9 estados")
+//         break;
+//     case "norte":
+//         console.log("Possui 7 estados")
+//         break;
+//     case "centro-oeste":
+//         console.log("Possui 3 estados")
+//         break;
+//     case "sudeste":
+//         console.log("Possui 4 estados")
+//         break;
+//     case "sul":
+//         console.log("Possui 3 estados")
+//         break;
+//     default:
+//         console.log("essa região não existe no Brasil")
+
+// }
+
+// let horario = 24
+
+// switch(horario){
+// case 0:
+// case 1:
+// case 2:
+// case 3:
+// case 4:
+// case 5:
+// case 24:
+//     console.log("Madrugada")
+//     break;
+
+// case 6:
+// case 7:
+// case 8:
+// case 9:
+// case 10:
+// case 11:
+//     console.log("Manhã")
+//     break;
+
+// case 12:
+// case 13:
+// case 14:
+// case 15:
+// case 16:
+// case 17:
+//     console.log("Tarde")
+//     break;
+// case 18:
+// case 19:
+// case 20:
+// case 21:
+// case 22:
+// case 23:
+//     console.log("Noite")
+//     break;
+
+// default:
+//     console.log("Insira um horário válido")
+//     break;
+// }
+
+// se igual menor zero ou menor igual a 0,499 = muito baixo
+// maior 0,499 menor igual 0,599 baixo
+// se maior 0,599 menor igual 0,699 Medio
+// se maior 0,699 menor igual 0,799 alto
+// se maior 0,799 menor igual 1,000 muito alto
+
+// let noruega = 0.944
+// let qatar = 0.850
+// let canada = 0.913
+// let brasil = 0.755
+// let japao = 0.891
+// let vietna = 0.666
+// let afeganistao = 0.465
+// let camaroes = 0.512
+
+// let idh = 3
+
+// if(idh >= 0 &&  idh < .5){
+//  console.log("O IDH é muito baixo")
+// }
+// else if (idh >=.5 && idh < .6){
+// console.log("O IDH é baixo")
+// }
+// else if (idh >=.6 && idh < .7){
+//     console.log ("O IDH é médio")
+// }
+// else if (idh >= .7 && idh <.8){
+//     console.log ("O IDH é alto")
+// }
+// else if (idh >= .8 && idh <=1){
+//     console.log ("O IDH é muito alto")
+// }
+// else{
+//     console.log ("IDH inválido")
+// }
+
+// const idh = [[noruega = 0.944],[qatar = 0.850],[canada = 0.913],[brasil = 0.755],[japao = 0.891],[ vietna = 0.666],[afeganistao = 0.465],[camaroes = 0.512]]
+// for(i=0; i< idh.length;i++){
+//     if(idh[i] >= 0 &&  idh[i] < .5){
+//         console.log( `O IDH ${idh[i]} é muito baixo` )
+//        }
+//        else if (idh[i] >=.5 && idh[i] < .6){
+//        console.log(`O IDH ${idh[i]} é baixo` )
+//        }
+//        else if (idh[i] >=.6 && idh[i] < .7){
+//            console.log (`O IDH ${idh[i]} é médio` )
+//        }
+//        else if (idh[i] >= .7 && idh[i] <.8){
+//            console.log (`O IDH ${idh[i]} é alto`)
+//        }
+//        else if (idh[i] >= .8 && idh[i] <=1){
+//            console.log (`O IDH ${idh[i]} é muito alto` )
+//        }
+//        else{
+//            console.log ("IDH inválido")
+//        }
+// }
+
+// const teste = [nome = "noruega", idh = 2]
+// for (i=0;i<teste.length;i++){
+//     if(teste[i] = 2){
+//         console.log(`o idh de ${nome} é ${idh}`)
+//     }
+// }
